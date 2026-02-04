df0bf943e72063f7ba93851cfff86d80dd9fda4d
Francisco Quintero <frankjrdev@gmail.com>
2026-02-04 08:16:01 +0100

    feat: add analytics, help, profile, projects, settings, and tasks components with templates and tests

---
+++ src/app/features/dashboard/pages/tasks/tasks.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tasks',
+  imports: [],
+  templateUrl: './tasks.html',
+  styleUrl: './tasks.scss',
+})
+export class Tasks {
+
+}
